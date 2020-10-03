0990100938434fd0575015380d3dc548e25a35f3
Ruben <ronatvik@gmail.com>
2020-10-03 13:45:03 +0200

    fixed bug in sp_crossover

--- src/implementation/mating/mating_functions.py
+++ src/implementation/mating/mating_functions.py
@@ -23,10 +23,10 @@ def sp_crossover(breeders: list, pop_size: int, ordered=False, fitness=False):
                 skip_counter += 1
                 index = (i + j + skip_counter) % num_breeders
             mate = breeders[index][0]
-            crossover_index = random.randint(1, len(breeders) - 2)
+            crossover_index = random.randint(0, len(breeders) - 1)
             child = breeder[0:crossover_index] + mate[crossover_index:]
             children.append(child)
+    child_surplus = len(children) - pop_size
+    for i in range(child_surplus):
+        children.pop(random.randint(0, len(children) - 1))
     return children
-
-
-
