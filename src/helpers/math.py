9b44435af986f939e2020f39bbf043989e529f75
Ruben <ronatvik@gmail.com>
2020-10-03 13:43:50 +0200

    Added constrain function

--- src/helpers/math.py
+++ src/helpers/math.py
@@ -1,3 +1,7 @@
 def lin_map(x, x0, x1, y0, y1):
     y = x * (y1 - y0) / (x1 - x0) + y0
     return y
+
+
+def constrain(value, min_value, max_value):
+    return min(max(value, min_value), max_value)
